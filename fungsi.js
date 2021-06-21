66d9f782c4d4454d042b83d011e339f2b6672ca6
Muhammad Dzaky <mdzaky07@gmail.com>
2021-06-21 21:37:42 +0700

    finish

--- fungsi.js
+++ fungsi.js
@@ -13,6 +13,13 @@ for (let i = 0; i < dataMhs.length; i++) {
     tbody.appendChild(clone);
 }
 
+function kosong() {
+    document.querySelector("input[name=npm]").value = "";
+    document.querySelector("input[name=nama]").value = "";
+    document.querySelector("input[name=alamat]").value = "";
+    document.querySelector("input[name=email]").value = "";
+}
+
 function addMhs() {
 
     var id = document.querySelectorAll("form[data-id]")[0].getAttribute("data-id");
@@ -28,25 +35,40 @@ function addMhs() {
         dataMhs[id][2] = alamat;
         dataMhs[id][3] = email;
 
-        document.querySelector("tr[data-id=id]").querySelector(".data-npm").innerText = npm;
+        document.querySelector(`tr[data-id="${id}"]`).querySelector(".data-npm").innerText = npm;
+        document.querySelector(`tr[data-id="${id}"]`).querySelector(".data-nama").innerText = nama;
+        document.querySelector(`tr[data-id="${id}"]`).querySelector(".data-alamat").innerText = alamat;
+        document.querySelector(`tr[data-id="${id}"]`).querySelector(".data-email").innerText = email;
         document.querySelectorAll("form[data-id]")[0].setAttribute("data-id", "");
+
+        kosong();
     } else {
-        var npm = document.querySelector("input[name=npm]").value;
-        var nama = document.querySelector("input[name=nama]").value;
-        var alamat = document.querySelector("input[name=alamat]").value;
-        var email = document.querySelector("input[name=email]").value;
-        dataMhs.push([npm, nama, alamat, email]);
-    
-        var tbody = document.querySelector("tbody");
-        var template = document.querySelector('#template');
-        var clone = template.content.cloneNode(true);
-        var td = clone.querySelectorAll("td");
-        clone.querySelectorAll("tr[data-id]")[0].setAttribute("data-id", dataMhs.length-1);
-        td[0].textContent = npm;
-        td[1].textContent = nama;
-        td[2].textContent = alamat;
-        td[3].textContent = email;
-        tbody.appendChild(clone);
+        var npm = document.querySelector("input[name=npm]").value == "";
+        var nama = document.querySelector("input[name=nama]").value == "";
+        var alamat = document.querySelector("input[name=alamat]").value == "";
+        var email = document.querySelector("input[name=email]").value == "";
+        if (npm && nama && alamat && email) {
+            alert("harap diisi");
+        } else {
+            var npm = document.querySelector("input[name=npm]").value;
+            var nama = document.querySelector("input[name=nama]").value;
+            var alamat = document.querySelector("input[name=alamat]").value;
+            var email = document.querySelector("input[name=email]").value;
+            dataMhs.push([npm, nama, alamat, email]);
+        
+            var tbody = document.querySelector("tbody");
+            var template = document.querySelector('#template');
+            var clone = template.content.cloneNode(true);
+            var td = clone.querySelectorAll("td");
+            clone.querySelectorAll("tr[data-id]")[0].setAttribute("data-id", dataMhs.length-1);
+            td[0].textContent = npm;
+            td[1].textContent = nama;
+            td[2].textContent = alamat;
+            td[3].textContent = email;
+            tbody.appendChild(clone);
+
+            kosong();
+        }
     }
 
 }
